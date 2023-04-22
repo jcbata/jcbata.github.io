13e3e2c55b5965d2aa4ad0d50bbe9917aaa9a4da
Julio César Bautista Tang <jcbauta@gmail.com>
2023-04-22 18:25:08 -0500

    Initial commit

---
+++ prediction7.js
@@ -0,0 +1,59 @@
+const predictButton = document.getElementById('predictButton');
+const inputImage = document.getElementById('inputImage');
+const imagePreview = document.getElementById('imagePreview');
+const predictionResult = document.getElementById('predictionResult');
+
+etiqueta = [
+    ['Ambulancia','La imágen es correcta, corresponde a un servicio de traslado de pacientes']
+    ,['Documento','Es probable que se observe su expediente, suba una otra imagen de fachada de su establecimiento']
+    ,['Establecimiento Urbano','La imagen es aceptable, Establecimiento en Zona Urbana']
+    ,['Establecimiento Rural','La imagen es aceptable, Establecimiento en Zona Rural']
+    ,['Edificio histórico','Es probable que su expediente sea observado, suba una imágen de un establecimiento de salud']
+    ,['Persona','Es probable que su expediente sea observado, suba otra imgaen de su establecimiento sin personas']
+    ,['Error','Es probable que su expediente sea observado, No se aprecia la fachada de su establecimiento de salud']
+]
+
+async function loadModel() {
+  // Cargar el modelo Keras desde un servidor remoto
+  console.log("Cargando modelo...");
+  model = await tf.loadGraphModel('./ipress_model3/model.json');
+  
+  console.log("Modelo cargado.");
+}
+loadModel();
+inputImage.addEventListener('change', function() {
+  const reader = new FileReader();
+  reader.onload = function() {
+    imagePreview.src = reader.result;
+    predictionResult.innerHTML = "";
+  }
+  reader.readAsDataURL(inputImage.files[0]);
+});
+
+predictButton.addEventListener('click', async function() {
+
+
+ 
+  // Preprocesar la imagen para adaptarla al modelo
+  const image = tf.browser.fromPixels(imagePreview);
+
+  //image.width = "250";
+  //image.height = "250";
+  processedImage = tf.cast( tf.image.resizeBilinear(image, [250, 250]),
+  'float32'
+  )
+  norm = tf.fill([250,250,3],255)
+  //processedImage = tf.div(processedImage,norm);
+  processedImage = tf.cast(tf.expandDims(processedImage),'float32');
+
+  // Realizar la predicción
+  const prediction = model.predict(processedImage);
+
+resultados = prediction.arraySync();
+ resu = resultados.indexOf(Math.max(resultados));
+  // Mostrar el resultado de la predicción
+  predictionResult.innerHTML = `${etiqueta[prediction.argMax(1).dataSync()[0]][0]} <br>
+    ${etiqueta[prediction.argMax(1).dataSync()[0]][1]}`;
+  console.log(prediction.dataSync());
+
+});
